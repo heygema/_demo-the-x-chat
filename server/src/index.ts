0779c0dd6160fd0082e5f9e15e312d09282ff071
Gema <gem.anggada@gmail.com>
2021-01-15 19:52:23 +0700

    refactor: from graphql-yoga -> apollo-server-express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,36 @@
-import {GraphQLServer} from 'graphql-yoga';
+import prisma from "./prismaClient";
+import path from "path";
+import express from "express";
+import http from "http";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Query {
+    hello(name: String): String!
+    messages(roomId: String!): [String!]!
+    friends: [String!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => {
+      return `hello ${name}`;
+    },
+    messages: () => {
+      return prisma.message.findMany();
+    },
+    friends: () => {
+      return [];
+    }
+  }
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+let app = express();
+app.use(express.static(path.join(__dirname, "../public")));
+apolloServer.applyMiddleware({ app });
+let server = http.createServer(app);
+server.listen(4000, () => {
+  console.log(`Server running on http://localhost${4000}`);
+});
